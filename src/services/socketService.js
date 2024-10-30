7e9fa87f3ada383c582dbfff97ac9fda5ff584a3
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-10-30 20:04:39 +0530

    messaging without database

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -62,9 +62,11 @@
 // };
 
 // module.exports = initializeSocket;
+
+// m-2
 const { Server } = require("socket.io");
 const prisma = require("../config/database");
-
+const newMessage = {};
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
@@ -104,38 +106,48 @@ const initializeSocket = (server) => {
       socket.join(roomId);
       console.log(`User with socket ID ${socket.id} joined room: ${roomId}`);
       socket.emit("room joined", roomId);
+      if (newMessage[roomId]) {
+        socket.emit('previous messages', newMessage[roomId]);
+      }
 
-      const previousMessages = await fetchPreviousMessages(roomId);
+      // const previousMessages = await fetchPreviousMessages(roomId);
       
-      socket.emit("previous messages", previousMessages);
+      if (newMessage[roomId]) {
+        socket.emit('previous messages', newMessage[roomId]);
+      }
     });
 
     socket.on("chat message", async ({ message, roomId }) => {
       console.log("Received message:", message, "for room:", roomId);
-
-      try {
-        const storedMessage = await prisma.chatMessage.create({
-          data: {
-            senderId: message.senderId,
-            sellerId: message.sellerid,
-            chatId: roomId,
-            text: message.text,
-
-            image: message.image,
-          },
-        });
-        // const messageWithTimestamp = {
-        //   ...storedMessage,
-        //   timestamp:storedMessage.createdAt,
-        // };
-
-        console.log("Message stored in MySQL database.");
-
-        io.to(roomId).emit("chat message", storedMessage);
-        io.to(roomId).emit("new message notification",storedMessage);
-      } catch (error) {
-        console.error("Error storing message in database:", error);
+      if (!newMessage[roomId]) {
+        newMessage[roomId] = [];
       }
+      newMessage[roomId].push(message);
+      io.to(roomId).emit("chat message", message);
+      io.to(roomId).emit("new message notification",message);
+      // try {
+      //   const storedMessage = await prisma.chatMessage.create({
+      //     data: {
+      //       senderId: message.senderId,
+      //       sellerId: message.sellerid,
+      //       chatId: roomId,
+      //       text: message.text,
+
+      //       image: message.image,
+      //     },
+      //   });
+      //   // const messageWithTimestamp = {
+      //   //   ...storedMessage,
+      //   //   timestamp:storedMessage.createdAt,
+      //   // };
+
+      //   console.log("Message stored in MySQL database.");
+
+        
+        
+      // } catch (error) {
+      //   console.error("Error storing message in database:", error);
+      // }
     });
 
     socket.on("disconnect", () => {
